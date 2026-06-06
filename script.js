4a13452d1a31c281fe5342f7e0df8013bfc41d50
Muhammad Mlahim <L1F22BSSE0281>
2026-06-06 16:54:07 +0500

    created and setup project of calculator

---
+++ script.js
@@ -0,0 +1,33 @@
+const display = document.getElementById("display");
+
+function appendValue(value){
+    display.value += value;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }catch{
+        display.value = "Error";
+    }
+}
+
+function squareRoot(){
+    display.value = Math.sqrt(display.value);
+}
+
+function square(){
+    display.value = Math.pow(display.value, 2);
+}
+
+function sin(){
+    display.value = Math.sin(display.value);
+}
+
+function cos(){
+    display.value = Math.cos(display.value);
+}
